4b748c1f5eddaf151efc1e27df72fb9964b4bb2d
edamararaya981 <edam_araya981@yahoo.com.ar>
2022-08-21 21:40:43 -0300

    cambio...
    cambio de los ...

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if (isset($_SESSION['nameUser'])) {
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>IL MERCATO</title>
+	<title>IL MERCATO...</title>
 	<link rel="stylesheet" type="text/css" href="Assets/css/bootstrap.min.css">
 </head>
 <body>
